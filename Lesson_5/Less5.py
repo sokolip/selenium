ce25341b6787aacc1021f24f7f49ceddae552e45
sokolip <sokolip8@gmail.com>
2023-11-13 22:10:40 -0500

    Lesson54

---
+++ Lesson_5/Less5.py
@@ -0,0 +1,21 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://hyperskill.org/tracks")
+
+time.sleep(3)
+
+print(driver.find_elements("class name", "nav-link"))
+
+driver.find_elements("class name", "nav-link")[2].click()
+
+time.sleep(3)
+
+
+
