421c51045778ce00a57538f0d7656b14ba1a8656
sokolip <sokolip8@gmail.com>
2023-11-16 08:58:05 -0500

    Lesson_9

--- Lesson_8/Find.py
+++ Lesson_8/Find.py
@@ -1,11 +1,4 @@
-import time
-
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-
-service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+c
 
 driver.get("https://freeconferencecall.com/global/pl")
 
--- Lesson_9/browers_option.py
+++ Lesson_9/browers_option.py
@@ -5,16 +5,19 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--incognito")
-chrome_options.add_argument("--ignor-certificate-errors")
+
+#chrome_options.add_argument("--headless") ,без запуска браузера
+#chrome_options.add_argument("--incognito") режим инкогнито
+#chrome_options.add_argument("--ignor-certificate-errors") обход ошибки сертификата SSL
 chrome_options.add_argument("--window-size=1920,1080")
-chrome_options.add_argument("--disable-cache")
+#chrome_options.add_argument("--disable-cache")
 
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service.chrome_options)
 
 driver.get("https://whatismyipaddress.com/")
+
+
 
 time.sleep(3)
 
