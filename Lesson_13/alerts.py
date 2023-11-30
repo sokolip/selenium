ab1c717f0af385071ffcf6f688d6bcfd1a9804de
sokolip <sokolip8@gmail.com>
2023-11-30 13:02:48 -0500

    Lesson_13

--- Lesson_13/alerts.py
+++ Lesson_13/alerts.py
@@ -5,14 +5,31 @@ from webdriver_manager.chrome import ChromeDriverManagerfrom selenium.webdriver.
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-options = options()
-options.add_argument("--window-size=1920.1080")
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument("--user-agent=Selenium")
-
-
 service = Service(ChromeDriverManager().install())
 driver = webdriver.chrome(service=service, options=options)
 wait = WebDriverWait(driver, 5, poll_frequency=1)
 
-driver.get("")
+driver.get("https://demoqa.com/alerts")
+
+BUTTON_1 = ("xpath", "//button[@id='alertButton']")
+wait.until(EC.element_to_be_clickable(BUTTON_1)).click()
+
+alert = wait.until(EC.alert_is_present())
+
+driver.switch_to.alert
+
+time.sleep(3)
+
+alert.accept()
+
+time.sleep(3)
+
+BUTTON_3 = ("xpath", "//button[@id='alertButton']")
+wait.until(EC.element_to_be_clickable(BUTTON_3)).click()
+time.sleep(3)
+print(alert.text)
+
+alert.dismiss()
+
+time.sleep(3)
+
